48e2e4c1d676965eb767ba284dd487b3a866ad32
Kexin-Luo <74885149+Kexin-Luo@users.noreply.github.com>
2023-05-19 21:03:34 +0800

    Add files via upload

---
+++ models/mobileNet.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from thop import profile
+class MobileNet(nn.Module):
+    def __init__(self, pretrained=False):
+        super(MobileNet, self).__init__()
+#         num_classes =4
+        self.model = models.mobilenet_v2()
+        print("mobilenet")
+#         self.model = models.squeezenet1_1(pretrained=pretrained)
+#         self.model = models.vgg16(pretrained=pretrained)
+#         print("vgg16")
+#         self.model.classifier = nn.Linear(1024, num_classes)
+        
+    def forward(self, x):
+        output = self.model(x)
+        return output
+if __name__ == '__main__':
+    model = MobileNet()
+    input = torch.randn(1, 3, 128, 250)
+    flops, params = profile(model, (input,))
+    print('flops: ', flops, 'params: ', params)
